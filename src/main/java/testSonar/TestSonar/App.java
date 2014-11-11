3861f5e488287aa049f79b9a22a93e7f087bcd84
estebanwasinger <estebanwasinger@gmail.com>
2014-11-11 12:51:34 -0300

    First Commit

---
+++ src/main/java/testSonar/TestSonar/App.java
@@ -0,0 +1,17 @@
+package testSonar.TestSonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static int hola(){
+    	return 4;
+    }
+}
